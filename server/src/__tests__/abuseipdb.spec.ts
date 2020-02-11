de9e5642334e82f4cb87471740e0dbc62b4f6f35
Andrew <andrewdymov@gmail.com>
2020-02-11 16:16:54 +0300

    Refactor abuseipdb a bit and improve test coverage

--- server/src/__tests__/abuseipdb.spec.ts
+++ server/src/__tests__/abuseipdb.spec.ts
@@ -14,6 +14,7 @@ dotenv.config()
 beforeEach(async done => {
     await redis.del('abuseipdb_222.186.42.155')
     await redis.del('abuseipdb_140.82.118.3')
+    await redis.del('abuseipdb_112.85.42.188')
 
     done()
 })
@@ -52,6 +53,43 @@ describe('send request with malicious IP, get response with automated check', fu
             .expect('Content-type', /html/)
             .expect(503)
             .expect(/Checking your browser before accessing the website/)
+
+        done()
+    }, 10_000)
+})
+
+describe('send request with malicious IP, get response with block', function() {
+    const app = express()
+
+    app.use(
+        umbress({
+            isProxyTrusted: true,
+            checkSuspiciousAddresses: {
+                enabled: true,
+                token: process.env.ABUSEIPDB_TOKEN,
+                action: 'block'
+            }
+        })
+    )
+
+    app.get('/', function(req, res) {
+        res.send('Access granted!')
+    })
+
+    it('should response with 503', async done => {
+        await request(app)
+            .get('/')
+            .set('X-Forwarded-For', '112.85.42.188')
+            .expect('Content-type', /html/)
+            .expect(200)
+            .expect('Access granted!')
+
+        await delay(5000)
+
+        await request(app)
+            .get('/')
+            .set('X-Forwarded-For', '112.85.42.188')
+            .expect(403)
 
         done()
     }, 10_000)
