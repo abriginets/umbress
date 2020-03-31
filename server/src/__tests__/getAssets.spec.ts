83b259908ef8fbe2ef2ba30227746936f91bf4a2
Andrew <andrewdymov@gmail.com>
2020-03-31 23:19:15 +0300

    Create getAssets.spec.ts

---
+++ server/src/__tests__/getAssets.spec.ts
@@ -0,0 +1,10 @@
+import { getPublicAsset } from '../helpers'
+
+describe('test faulty code', function() {
+    it('should throw because unexistent file', function() {
+        expect(() =>
+            //@ts-ignore
+            getPublicAsset('cwmthet', 'dfg')
+        ).toThrow()
+    })
+})
